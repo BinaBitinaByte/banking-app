9c7dc5659f5480889fe7428fb7cddf0824d1d880
DevMountain <devmountain@DevMountains-MacBook-Pro.local>
2019-04-25 09:53:05 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const massive = require('massive');
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+
+massive(process.env.CONNECTION_STRING)
+.then(db => {
+    app.set('db', db);
+    console.log('Database Connected');
+}).catch(err => {
+    console.log(err);
+})
+
+
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
+
+
+app.listen(5050, () => console.log('Listening on Port 5050'))
